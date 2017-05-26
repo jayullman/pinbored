6fab834bb0f43a9ddeed9daae79f3a28c10cbd0a
Jay Ullman <jayullman@gmail.com>
2017-05-26 15:05:03 -0700

    Install dependencies and add server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App is listening on port: ${ port }`);
+});
