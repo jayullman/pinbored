cc9e668067825618fbc959be79cdb94856fa3df9
Jay Ullman <jayullman@gmail.com>
2017-05-26 22:39:53 -0700

    Add database connection util

---
+++ server/utils/connectDb.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const url = process.env.MONGOLAB_URI;
+const db = mongoose.connection;
+const options = {
+  server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
+};
+
+module.exports = function () {
+  mongoose.connect(url, options);
+  db.on('open', () => {
+    console.log('Connected to database');
+  });
+}
