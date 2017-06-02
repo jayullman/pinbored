cc8735fadc43e4698b5ab585e5bbc01d2ab5a94a
Jay Ullman <jayullman@gmail.com>
2017-06-01 22:23:11 -0700

    Add modernizr package

--- client/index.js
+++ client/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './containers/App';
 
+// adds classes to touch and non-touch devices for css targeting
+import './modernizr-touch';
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
